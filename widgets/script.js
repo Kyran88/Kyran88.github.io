eaea35f9817f778d1d96d912c70b8e422cc9bdbe
Kyran <kmcmanus09@hotmail.com>
2018-06-10 12:01:47 +0100

    Update script.js

--- widgets/script.js
+++ widgets/script.js
@@ -58,6 +58,21 @@ $(document).ready(function(){
         });
 });
 
+//Get location
+        $.ajax({
+            url: "https://geoip-db.com/jsonp",
+            jsonpCallback: "callback",
+            dataType: "jsonp",
+            success: function( location ) {
+                $('#country').html(location.country_name);
+                $('#state').html(location.state);
+                $('#city').html(location.city);
+                $('#latitude').html(location.latitude);
+                $('#longitude').html(location.longitude);
+                $('#ip').html(location.IPv4);  
+            }
+        });  
+
 
 
 var app = angular.module("myApp", []);
