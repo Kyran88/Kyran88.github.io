7066ef611fe97a225f686cb04df7dd5826df9aef
Kyran <kmcmanus09@hotmail.com>
2018-06-13 12:45:56 +0100

    Create getweather.js

---
+++ bot/webapp/js/getweather.js
@@ -0,0 +1,43 @@
+// Docs at http://simpleweatherjs.com
+
+/* Does your browser support geolocation? */
+if ("geolocation" in navigator) {
+  $('.js-geolocation').show(); 
+} else {
+  $('.js-geolocation').hide();
+}
+
+/* Where in the world are you? */
+$('.js-geolocation').on('click', function() {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    loadWeather(position.coords.latitude+','+position.coords.longitude); //load weather using your lat/lng coordinates
+  });
+});
+
+/* 
+* Test Locations
+* Perth lat/long: 56.3950,3.4308
+* Perth WOEID: 27760678
+*/
+$(document).ready(function() {
+  loadWeather('Perth scotland',''); //@params location, woeid
+});
+
+function loadWeather(location, woeid) {
+  $.simpleWeather({
+    location: location,
+    woeid: woeid,
+    unit: 'C',
+    success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.alt.temp+'&deg;f</li></ul>';  
+      
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+}
