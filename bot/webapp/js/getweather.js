2a1d254b694627edbd226da9e44a4fe3110b7945
Kyran <kmcmanus09@hotmail.com>
2018-06-13 13:46:39 +0100

    Update getweather.js

--- bot/webapp/js/getweather.js
+++ bot/webapp/js/getweather.js
@@ -1,67 +1,50 @@
-(function($) {
+$(document).ready(function() {
 
-  // Following script makes use of following services:
-  // http://ipinfo.io/ (geo data)
-  // http://openweathermap.org/ (weather data)
+  var long;
+  var lat;
+  var celsius;
+  var fahrenheit;
+  var tempSwap = true;
 
-  $(document).ready(function() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
 
-    var $city             = $('.js-city'),
-        $country          = $('.js-country'),
-        $temperature      = $('.js-temp'),
-        $temperatureUnit  = $('.js-temp-unit'),
-        $weather          = $('.js-weather'),
-        $weatherIcon      = $('.js-weather-icon');
+      var api = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(SELECT%20woeid%20FROM%20geo.places%20WHERE%20text%3D%22(' + lat + '%2C' + long + ')%22)%20and%20u%3D\'c\'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+     console.log(api);
 
-    // Get geo data
-    $.getJSON('https://ipinfo.io', function() {
-    }).done(function(geoJSON) {
+      $.getJSON(api, function(data) {
+        //alert(data.query.lang);
+        var city = data.query.results.channel.location.city;
+        celsius = data.query.results.channel.item.condition.temp;
+        fahrenheit = Math.round(celsius * 1.8 + 32);
+        var weatherType = data.query.results.channel.item.condition.text.toLowerCase();
+        var weatherCode = data.query.results.channel.item.condition.code;
 
-      var city              = geoJSON.city,
-          country           = geoJSON.country,
-          openWeatherAPPID  = '57d6d3575458519af4e1386b34a7ac48', // :(
-          openWeatherURL    = '//api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country + '&appid=' + openWeatherAPPID;
-
-      // Fill in city and country
-      $city.text(city);
-      $country.text(country);
-
-      // Get weather data
-      $.getJSON(openWeatherURL, function() {
-      }).done(function(weatherJSON) {
-
-        var kelvin    = weatherJSON.main.temp,
-            farenheit = kelvinToFarenheit(kelvin),
-            celcius   = kelvinToCelcius(kelvin),
-            weather   = weatherJSON.weather[0].main,
-            icon      = weatherJSON.weather[0].icon;
-
-        // Fill in temperature and weather info
-        $temperature.text(celcius); // Default to celcius
-        $weather.text(weather);
-        $weatherIcon.html(function() {
-          return '<img src="http://openweathermap.org/img/w/' + icon + '.png" alt="Weather Icon">';
-        });
-
-        // Fahrenheit / Celcius Toggle
-        $temperatureUnit.on('click', function(e) {
-          e.preventDefault();
-          var $this = $(this);
-          if ($this.text() === 'F') {
-            // Convert to Celcius
-            $this.text('C');
-            $temperature.text(celcius);
+        $('#js-city').html(city);
+        $('#js-celsius').html(celsius + " °C");
+        $('#js-fahrenheit').html(fahrenheit);
+        $('#js-weather-type').html(weatherType);
+        $('#js-weather-icon').html('<i class="wi wi-yahoo-' + weatherCode + '"></i>');
+        $("#js-temp-change").html("fahrenheit");
+        $('#js-temp-swap-button').click(function() {
+          if (tempSwap === false) {
+            $("#js-celsius").html(celsius + " °C");
+            $("#js-temp-change").html("fahrenheit");
+            tempSwap = true;
           } else {
-            // Convert to Farenheit
-            $this.text('F');
-            $temperature.text(farenheit);
+            $("#js-celsius").html(fahrenheit + " °F");
+            $("#js-temp-change").html("celsius");
+            tempSwap = false;
           }
+          
         });
 
       });
     });
-
-  });
+  }
+});
   
   function kelvinToFarenheit(kelvin) {
     return parseInt((kelvin - 273.15) * (9 / 5) + 32, 10);
