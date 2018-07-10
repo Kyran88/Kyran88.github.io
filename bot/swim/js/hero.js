e801de654875d4611ade4e23d005932eb8e3c0d2
Kyran <kmcmanus09@hotmail.com>
2018-07-10 20:31:40 +0100

    Update hero.js

--- bot/swim/js/hero.js
+++ bot/swim/js/hero.js
@@ -1,40 +1,14 @@
-$(function() {
-  $(".p2").typed({
-    strings: ["COACH", "INSPIRATION", "MOTIVATION"],
-    typeSpeed: 50,
-    backSpeed: 10,
-    backDelay: 2000,
-    showCursor: false,
-    loop: false
-  });
+var colorPickers = document.querySelectorAll('.colors li');
+
+[].forEach.call(colorPickers, function (picker)
+{
+    picker.addEventListener('click', changeColor)
 });
 
-$('.hero-down').mousedown(function() {
-  TweenMax.fromTo('.btn-react', 0.25, {
-    opacity: 0,
-    scale: 0
-  }, {
-    opacity: 0.25,
-    scale: 1,
-    onComplete: function() {
-      TweenMax.to('.btn-react', 0.25, {
-        opacity: 0,
-        scale: 0
-      });
-    }
-  });
-});
-
-// smooth scroll to div
-$('a[href*=#]:not([href=#])').click(function() {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html,body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-      return false;
-    }
-  }
-});
+function changeColor()
+{
+  var colorValue = window.getComputedStyle(this, null).backgroundColor;
+  var tintColor = colorValue.replace(')', ', .6)');
+  tintColor = tintColor.replace('rgb', 'rgba');
+  document.querySelector('.tint').style.backgroundColor= tintColor;
+}
